8363f63be3723904db264e2d589023581fb63e7f
Mateo Greil <mateo.greil@gmail.com>
2019-11-26 21:01:52 +0000

    ajout script sh qui fait la meme

---
+++ ip_tracker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PUBLIC_IP_FILE=/home/mat/IPTracker/.ip
+PUBLIC_IP=`wget http://ipecho.net/plain -O - -q ; echo`
+
+read last_ip < $PUBLIC_IP_FILE
+if [ $last_ip != $PUBLIC_IP ]
+then
+	echo "L'ip a change de " + $last_ip + " vers " + $PUBLIC_IP
+	sendemail -l ~/email.log -f ${MAIL_FROM} -u "L'ip de l'appart a change !" -t ${MAIL_TO} -s "smtp.gmail.com:587"  -o tls=yes -xu ${MY_EMAIL} -xp ${MY_EMAIL_PASSWORD} -m "Attention ! N'utilise plus : " $last_ip ". Voici la nouvelle adresse : " $PUBLIC_IP
+
+	echo $PUBLIC_IP > $PUBLIC_IP_FILE
+else
+	echo 'Pas de difference'
+fi
